8657d2afff6995b38fbf7e7fd42041e108bf2e3d
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-05 11:57:49 +0600

    created Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,8 +4,17 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
-        <span className="navbar-brand"><NavLink className="nav-link" to="/">TV shows</NavLink></span>
+        <span className="navbar-brand">HW-70</span>
         <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">Contacts</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/new-contact">Add new contact</NavLink>
+            </li>
+
+          </ul>
         </div>
       </div>
     </nav>
