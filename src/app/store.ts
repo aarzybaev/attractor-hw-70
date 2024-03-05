ee9132ce74222471cc78c189f473bbbb28fd4e5f
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-05 11:59:28 +0600

    created store file

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
+import {contactsReducer} from '../store/contactSlice';
 
 export const store = configureStore({
 
   reducer: {
+    contacts: contactsReducer
   }
 
 });
