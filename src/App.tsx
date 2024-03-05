be3ce1057677c71b02f619c0f8e53155aeea4d34
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-05 12:00:22 +0600

    created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {Route, Routes} from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import Contacts from './containers/Contacts/Contacts';
+import ContactForm from './components/ContactForm/ContactForm';
 
 const App = () => (
   <>
     <Layout>
       <Routes>
-        <Route path="/" element={(<></>)}/>
+        <Route path="/" element={(<Contacts/>)} />
+        <Route path="/new-contact" element={(<ContactForm/>)}/>
+        <Route path="/new-contact/:id" element={(<ContactForm/>)}/>
         <Route path="*" element={<h4>Oops! Page not found...</h4>}/>
       </Routes>
     </Layout>
